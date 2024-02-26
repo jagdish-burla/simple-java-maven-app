604495f4427db39aafce75ef3d3cca712e3e243c
Jagdish Vamsi <jagdishvamsi@Jagdishs-MacBook-Air.local>
2024-02-26 22:34:05 +0530

    Added a new text

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Jagdish!";
+    private static final String MESSAGE = "Hello Jagdish@123!";
 
     public App() {}
 
